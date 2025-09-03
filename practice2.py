2381a0c8730783cbe99c9c155a9fd402cab7d1c2
mbrahm01 <137713388+mbrahm01@users.noreply.github.com>
2025-09-03 11:40:49 +0530

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,7 +1,7 @@
 import requests
 import json
-API_KEY = "AIzaSyDrmTbgZAjw-zXILKfQwEutXapI3bwecaw"
-SEARCH_ENGINE_ID = "8602b4c46bda04185"
+API_KEY = ""
+SEARCH_ENGINE_ID = ""
 query = '''("data engineer" OR "ML engineer") AND ("python" OR "tensorflow") AND ("San Francisco" OR "California") AND ("Google" OR "Meta") AND (gmail OR "@gmail.com" OR "@outlook.com") AND (phone OR "contact" OR "mobile") -intitle:"profiles" -inurl:"dir/" site:linkedin.com/in/ OR site:linkedin.com/pub/'''
 
 url = f"https://www.googleapis.com/customsearch/v1"
@@ -17,4 +17,5 @@ results = response.json()
 with open('data.json', 'w') as json_file:
     json.dump(results, json_file, indent=4)
 print(results)
+
 
